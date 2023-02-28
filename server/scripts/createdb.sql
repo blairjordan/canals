0abe8a3098a0626b866656739b4f8f3b1fd86db0
Blair Jordan <blair.j.jordan@gmail.com>
2023-02-28 23:53:50 +0800

    chore: add vendors, rename objects

--- server/scripts/createdb.sql
+++ server/scripts/createdb.sql
@@ -1,4 +1,4 @@
-CREATE TABLE players (
+CREATE TABLE IF NOT EXISTS players (
   id BIGSERIAL PRIMARY KEY,
   username TEXT NOT NULL,
   meta JSONB,
@@ -6,33 +6,25 @@ CREATE TABLE players (
   balance NUMERIC(10, 2) NOT NULL DEFAULT 0.00
 );
 
-CREATE TABLE grid_nodes (
+CREATE TABLE IF NOT EXISTS markers (
   id BIGSERIAL PRIMARY KEY,
   position JSONB NOT NULL,
   type VARCHAR(55),
   props JSONB
 );
 
-CREATE INDEX grid_nodes_position_idx ON grid_nodes USING GIN (position);
-CREATE INDEX idx_grid_node_type ON grid_nodes (type);
+CREATE INDEX IF NOT EXISTS markers_position_idx ON markers USING GIN (position);
+CREATE INDEX IF NOT EXISTS idx_markers_type ON markers (type);
 
-INSERT INTO grid_nodes (position) VALUES ('{"x": 0, "y": 0, "z": 0}');
-INSERT INTO grid_nodes (position) VALUES ('{"x": 500, "y": 0, "z": 0}');
-INSERT INTO grid_nodes (position) VALUES ('{"x": 500, "y": 0, "z": 500}');
-
-CREATE TABLE grid_node_links (
+CREATE TABLE IF NOT EXISTS links (
   id BIGSERIAL PRIMARY KEY,
-  from_grid_node_id BIGINT REFERENCES grid_nodes(id) NOT NULL,
-  to_grid_node_id BIGINT REFERENCES grid_nodes(id) NOT NULL,
-  props JSONB
+  from_marker_id BIGINT REFERENCES markers(id) NOT NULL,
+  to_marker_id BIGINT REFERENCES markers(id) NOT NULL,
+  props JSONB,
+  CONSTRAINT uq_links UNIQUE (from_marker_id, to_marker_id)
 );
 
-ALTER TABLE grid_node_links ADD CONSTRAINT uq_links UNIQUE (from_grid_node_id, to_grid_node_id);
-
-INSERT INTO grid_node_links (from_grid_node_id, to_grid_node_id) VALUES (1, 2);
-INSERT INTO grid_node_links (from_grid_node_id, to_grid_node_id) VALUES (1, 3);
-
-CREATE TABLE items (
+CREATE TABLE IF NOT EXISTS items (
   id BIGSERIAL PRIMARY KEY,
   name TEXT NOT NULL,
   type VARCHAR(55),
@@ -40,14 +32,27 @@ CREATE TABLE items (
   floor_price NUMERIC(10,2) DEFAULT NULL
 );
 
-CREATE TABLE grid_node_items (
-  grid_node_id BIGINT REFERENCES grid_nodes(id) NOT NULL,
+CREATE TABLE IF NOT EXISTS marker_items (
+  marker_id BIGINT REFERENCES markers(id) NOT NULL,
   item_id BIGINT REFERENCES items(id) NOT NULL
 );
 
+INSERT INTO markers (position, type)
+VALUES
+  ('{"x": 0, "y": 0, "z": 0}', 'geo_marker'),
+  ('{"x": 500, "y": 0, "z": 0}', 'geo_marker'),
+  ('{"x": 500, "y": 0, "z": 500}', 'geo_marker')
+ON CONFLICT DO NOTHING;
+
+INSERT INTO links (from_marker_id, to_marker_id)
+VALUES
+(1, 2),
+(1, 3)
+ON CONFLICT DO NOTHING;
+
 -- 🎣 Fishing vendor
 WITH vendor_insert AS (
-  INSERT INTO grid_nodes (position, type, props)
+  INSERT INTO markers (position, type, props)
   VALUES ('{"x": 50, "y": 50, "z": 0}', 'vendor', '{"name": "Bob''s Bait''n''Tackle Shop"}')
   RETURNING id
 ),
@@ -60,12 +65,12 @@ item_insert AS (
   ('Fly Rod', 'A specialized fishing rod designed for fly fishing.', 200.00, 'fishing_rod')
   RETURNING id
 )
-INSERT INTO grid_node_items (grid_node_id, item_id)
+INSERT INTO marker_items (marker_id, item_id)
 SELECT vendor_insert.id, item_insert.id FROM vendor_insert, item_insert;
 
 -- 🪴 Florist vendor
 WITH vendor_insert AS (
-  INSERT INTO grid_nodes (position, type, props)
+  INSERT INTO markers (position, type, props)
   VALUES ('{"x": 75, "y": 75, "z": 0}', 'vendor', '{"name": "Mary''s Florist"}')
   RETURNING id
 ),
@@ -78,7 +83,47 @@ item_insert AS (
   ('Climbing Ivy', 'A potted Climbing Ivy, trained to climb a trellis or wall.', 25.00, 'plant')
   RETURNING id
 )
-INSERT INTO grid_node_items (grid_node_id, item_id)
+INSERT INTO marker_items (marker_id, item_id)
+SELECT vendor_insert.id, item_insert.id FROM vendor_insert, item_insert;
+
+-- 🛥 Boating vendor
+WITH vendor_insert AS (
+  INSERT INTO markers (position, type, props)
+  VALUES ('{"x": 50, "y": 50, "z": 0}', 'vendor', '{"name": "Frank''s Boating"}')
+  RETURNING id
+),
+item_insert AS (
+  INSERT INTO items (name, description, floor_price, type)
+  VALUES
+  -- General Items
+  ('Air Conditioner', 'Cools air inside a boat''s cabin or enclosed space.', 500.00, 'general_item'),
+  ('Solar Panel', 'Converts sunlight into electricity to power onboard systems.', 300.00, 'general_item'),
+  ('Windlass', 'Mechanical device for lifting heavy loads using a crank or handle.', 1000.00, 'general_item'),
+  ('Marine Radio', 'Two-way radio for communication on water.', 200.00, 'general_item'),
+  ('Docking Lines', 'Strong, flexible ropes used to secure the boat to a dock or fixed object.', 75.00, 'general_item'),
+  -- Hulls
+  ('Flat-bottomed Hull', 'A type of boat hull that has a flat bottom, making it very stable but slower than other hull types.', 500.00, 'boat_hull'),
+  ('Multi-chine Hull', 'A type of boat hull that has multiple angles or "chines" in its shape, providing good stability and speed.', 1000.00, 'boat_hull'),
+  ('Round-bottomed Hull', 'A type of boat hull that has a round bottom, providing good speed but less stability than flat-bottomed hulls.', 1500.00, 'boat_hull'),
+  -- Decks
+  ('Fiberglass Deck', 'A type of boat deck made from fiberglass, providing good durability and resistance to water damage.', 750.00, 'boat_deck'),
+  ('Pine Deck', 'A type of boat deck made from pine wood, providing a traditional and classic look.', 500.00, 'boat_deck'),
+  ('Douglas Fir Deck', 'A type of boat deck made from Douglas fir wood, providing good strength and durability.', 1000.00, 'boat_deck'),
+  ('Cedar Deck', 'A type of boat deck made from cedar wood, providing good resistance to water damage and a pleasant aroma.', 1250.00, 'boat_deck'),
+  ('Teak Deck', 'A type of boat deck made from teak wood, providing excellent durability and resistance to water damage.', 2000.00, 'boat_deck'),
+  ('Mahogany Deck', 'A type of boat deck made from mahogany wood, providing a luxurious and classic look.', 2500.00, 'boat_deck'),
+  -- Engines
+  ('Outboard Engine', 'A type of boat engine that is mounted on the outside of the boat, providing good speed and maneuverability.', 5000.00, 'boat_engine'),
+  ('Inboard Engine', 'A type of boat engine that is mounted inside the boat, providing good power and torque.', 7500.00, 'boat_engine'),
+  ('Electric Engine', 'A type of boat engine that is powered by electricity, providing quiet and eco-friendly propulsion.', 10000.00, 'boat_engine'),
+  ('Inboard Diesel Engine', 'A type of boat engine that is powered by diesel fuel and mounted inside the boat, providing good power and fuel efficiency.', 12500.00, 'boat_engine'),
+  -- Sterns
+  ('Traditional', 'A classic stern design that provides good stability and handling.', 500.00, 'boat_stern'),
+  ('Semi-Traditional', 'A modern take on the traditional stern design, providing a balance between stability and speed.', 2500.00, 'boat_stern'),
+  ('Cruiser', 'A sleek and stylish stern design that emphasizes speed and maneuverability.', 5000.00, 'boat_stern')
+  RETURNING id
+)
+INSERT INTO marker_items (marker_id, item_id)
 SELECT vendor_insert.id, item_insert.id FROM vendor_insert, item_insert;
 
 -- PostGraphile GQL subscbription for player updates
@@ -110,35 +155,34 @@ CREATE TRIGGER player_changes_trigger
   EXECUTE FUNCTION notify_player_changes();
 
 -- A nice view to return the grid node links recursively 👀
-CREATE VIEW grid_node_links_recursive AS
-WITH RECURSIVE grid_node_links_recursive AS (
+CREATE VIEW links_recursive AS
+WITH RECURSIVE links_recursive AS (
     -- 🛑 Non-recursive term
     SELECT
       id,
-      from_grid_node_id,
-      to_grid_node_id,
+      from_marker_id,
+      to_marker_id,
       props,
       1 as depth
-    FROM grid_node_links
-    WHERE from_grid_node_id = 1
+    FROM links
+    WHERE from_marker_id = 1
     UNION ALL
     -- 🪃 Recursive term
     SELECT
       gnl.id,
-      gnl.from_grid_node_id,
-      gnl.to_grid_node_id,
+      gnl.from_marker_id,
+      gnl.to_marker_id,
       gnl.props,
       gnlr.depth + 1 AS depth
-    FROM grid_node_links gnl
-    INNER JOIN grid_node_links_recursive gnlr ON gnlr.to_grid_node_id = gnl.from_grid_node_id
+    FROM links gnl
+    INNER JOIN links_recursive gnlr ON gnlr.to_marker_id = gnl.from_marker_id
 )
 SELECT
   id,
-  from_grid_node_id,
-  to_grid_node_id,
+  from_marker_id,
+  to_marker_id,
   props,
   depth
-FROM grid_node_links_recursive;
+FROM links_recursive;
 
-COMMENT ON VIEW grid_node_links_recursive IS E'@foreignKey (to_grid_node_id) references grid_nodes (id)|@foreignFieldName toGridNode\n@foreignKey (from_grid_node_id) references grid_nodes (id)|@foreignFieldName fromGridNode';
-
+COMMENT ON VIEW links_recursive IS E'@foreignKey (to_marker_id) references markers (id)|@foreignFieldName toMarker\n@foreignKey (from_marker_id) references markers (id)|@foreignFieldName fromMarker';
