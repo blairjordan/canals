327286768cf07db7aef7d0d7ecb831bfa1899d91
Blair Jordan <blair.j.jordan@gmail.com>
2023-02-28 21:06:06 +0800

    feat: add vendors and associated grid items

--- server/scripts/createdb.sql
+++ server/scripts/createdb.sql
@@ -2,7 +2,8 @@ CREATE TABLE players (
   id BIGSERIAL PRIMARY KEY,
   username TEXT NOT NULL,
   meta JSONB,
-  position JSONB
+  position JSONB,
+  balance NUMERIC(10, 2) NOT NULL DEFAULT 0.00
 );
 
 CREATE TABLE grid_nodes (
@@ -12,6 +13,7 @@ CREATE TABLE grid_nodes (
   props JSONB
 );
 
+CREATE INDEX grid_nodes_position_idx ON grid_nodes USING GIN (position);
 CREATE INDEX idx_grid_node_type ON grid_nodes (type);
 
 INSERT INTO grid_nodes (position) VALUES ('{"x": 0, "y": 0, "z": 0}');
@@ -29,6 +31,55 @@ ALTER TABLE grid_node_links ADD CONSTRAINT uq_links UNIQUE (from_grid_node_id, t
 
 INSERT INTO grid_node_links (from_grid_node_id, to_grid_node_id) VALUES (1, 2);
 INSERT INTO grid_node_links (from_grid_node_id, to_grid_node_id) VALUES (1, 3);
+
+CREATE TABLE items (
+  id BIGSERIAL PRIMARY KEY,
+  name TEXT NOT NULL,
+  type VARCHAR(55),
+  description TEXT,
+  floor_price NUMERIC(10,2) DEFAULT NULL
+);
+
+CREATE TABLE grid_node_items (
+  grid_node_id BIGINT REFERENCES grid_nodes(id) NOT NULL,
+  item_id BIGINT REFERENCES items(id) NOT NULL
+);
+
+-- 🎣 Fishing vendor
+WITH vendor_insert AS (
+  INSERT INTO grid_nodes (position, type, props)
+  VALUES ('{"x": 50, "y": 50, "z": 0}', 'vendor', '{"name": "Bob''s Bait''n''Tackle Shop"}')
+  RETURNING id
+),
+item_insert AS (
+  INSERT INTO items (name, description, floor_price, type)
+  VALUES
+  ('Spincast Rod', 'A simple fishing rod designed to work with spincasting reels.', 25.00, 'fishing_rod'),
+  ('Spinning Rod', 'The most common type of fishing rod, designed to work with spinning reels.', 50.00, 'fishing_rod'),
+  ('Baitcasting Rod', 'A powerful fishing rod designed to work with baitcasting reels.', 100.00, 'fishing_rod'),
+  ('Fly Rod', 'A specialized fishing rod designed for fly fishing.', 200.00, 'fishing_rod')
+  RETURNING id
+)
+INSERT INTO grid_node_items (grid_node_id, item_id)
+SELECT vendor_insert.id, item_insert.id FROM vendor_insert, item_insert;
+
+-- 🪴 Florist vendor
+WITH vendor_insert AS (
+  INSERT INTO grid_nodes (position, type, props)
+  VALUES ('{"x": 75, "y": 75, "z": 0}', 'vendor', '{"name": "Mary''s Florist"}')
+  RETURNING id
+),
+item_insert AS (
+  INSERT INTO items (name, description, floor_price, type)
+  VALUES
+  ('Snake Plant', 'A potted Snake Plant, also known as Mother-in-Law’s Tongue.', 20.00, 'plant'),
+  ('Barrel Cactus', 'A potted Barrel Cactus, also known as Ferocactus.', 60.00, 'plant'),
+  ('Boxwood Topiary', 'A potted Boxwood Topiary, pruned into a ball shape.', 35.00, 'plant'),
+  ('Climbing Ivy', 'A potted Climbing Ivy, trained to climb a trellis or wall.', 25.00, 'plant')
+  RETURNING id
+)
+INSERT INTO grid_node_items (grid_node_id, item_id)
+SELECT vendor_insert.id, item_insert.id FROM vendor_insert, item_insert;
 
 -- PostGraphile GQL subscbription for player updates
 CREATE OR REPLACE FUNCTION notify_player_changes()
@@ -89,5 +140,5 @@ SELECT
   depth
 FROM grid_node_links_recursive;
 
-COMMENT ON VIEW grid_node_links_recursive IS E'@foreignKey (to_grid_node_id) references grid_node (id)|@foreignFieldName toGridNode\n@foreignKey (from_grid_node_id) references grid_node (id)|@foreignFieldName fromGridNode';
+COMMENT ON VIEW grid_node_links_recursive IS E'@foreignKey (to_grid_node_id) references grid_nodes (id)|@foreignFieldName toGridNode\n@foreignKey (from_grid_node_id) references grid_nodes (id)|@foreignFieldName fromGridNode';
 
