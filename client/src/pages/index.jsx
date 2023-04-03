3bda964977206e1334c9397d90517d887822bc21
Blair Jordan <blair.j.jordan@gmail.com>
2023-04-04 00:02:29 +0800

    some ui placeholder stuff

--- client/src/pages/index.jsx
+++ client/src/pages/index.jsx
@@ -1,5 +1,4 @@
 import dynamic from 'next/dynamic'
-import Instructions from '@/components/dom/Instructions'
 
 // Dynamic import is used to prevent a payload when the website starts, that includes threejs, r3f etc..
 // WARNING ! errors might get obfuscated by using dynamic import.
@@ -10,11 +9,11 @@ const Logo = dynamic(() => import('@/components/canvas/Logo'), { ssr: false })
 // Dom components go here
 export default function Page(props) {
   return (
-    <Instructions>
+    <>
       This is a minimal starter for Nextjs + React-three-fiber and Threejs. Click on the{' '}
       <span className='text-cyan-200'>atoms nucleus</span> to navigate to the{' '}
       <span className='text-green-200'>/blob</span> page. OrbitControls are enabled by default.
-    </Instructions>
+    </>
   )
 }
 
