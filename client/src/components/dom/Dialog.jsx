e3b70414668644e197e936e3658452c0a53bfd96
Blair Jordan <blair.j.jordan@gmail.com>
2023-04-04 16:23:10 +0800

    feat: add ui elements

---
+++ client/src/components/dom/Dialog.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Dialog = ({ avatarUrl, name, children }) => {
+  return (
+    <div className="flex items-start py-4">
+      <div className="h-12 w-12 rounded-full bg-gray-300 mr-4"></div>
+      <div className="bg-gray-200 rounded-lg px-4 py-3">
+        <p className="font-semibold text-gray-800">{name}</p>
+        <div className="text-gray-800">{children}</div>
+      </div>
+    </div>
+  );
+};
+
+export default Dialog;
