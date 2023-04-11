cdc740d62948829940f4e6536284aa4b3ea56404
blair <blairjordan@users.noreply.github.com>
2023-04-12 01:29:22 +0800

    feat: add item purchase functionality (#50)

--- client/src/pages/index.jsx
+++ client/src/pages/index.jsx
@@ -2,64 +2,81 @@ import dynamic from 'next/dynamic'
 import { useState, useEffect, useCallback } from 'react'
 import Popup from '@/components/dom/Popup'
 import Login from '@/components/dom/Login'
-import { useAppContext } from '@/context';
-import { useLazyQuery, useSubscription } from '@apollo/client';
-import { PLAYER, PLAYERS_ALL, PLAYERS_NEARBY } from '@/graphql/player';
-import ItemGrid from '@/components/dom/ItemGrid';
+import { useAppContext } from '@/context'
+import { useLazyQuery, useSubscription, useMutation } from '@apollo/client'
+import { PLAYER, PLAYERS_ALL, PLAYERS_NEARBY } from '@/graphql/player'
+import { PURCHASE } from '@/graphql/action'
+import ItemGrid from '@/components/dom/ItemGrid'
 
 const Game = dynamic(() => import('@/components/canvas/Game'), { ssr: false })
 
 export default function Page(props) {
-  const [state, dispatch] = useAppContext();
-  const [showLogin, setShowPopup] = useState(false) // Add state to manage popup visibility
+  const [state, dispatch] = useAppContext()
 
-  const [getPlayer, { loading: loadingPlayer, data: playerData, error: playerError }] = useLazyQuery(PLAYER);
-  const [getRemotePlayers, { loading: loadingRemotePlayers, data: remotePlayersData, error: remotePlayersError }] = useLazyQuery(PLAYERS_ALL);
-  
+  const [getPlayer, { loading: loadingPlayer, data: playerData, error: playerError }] = useLazyQuery(PLAYER, { fetchPolicy: 'no-cache' })
+  const [getRemotePlayers, { loading: loadingRemotePlayers, data: remotePlayersData, error: remotePlayersError }] =
+    useLazyQuery(PLAYERS_ALL)
+  const [purchaseItem] = useMutation(PURCHASE, {
+    onCompleted: (data) => {
+      console.log('Item purchased successfully:', data)
+      // Refresh player's items
+      getPlayer({ variables: { id: state.player.id } })
+    },
+    onError: (error) => {
+      console.log('Error purchasing item:', error)
+    },
+  })
+
   useSubscription(PLAYERS_NEARBY, {
     onData: (payload) => {
       if (!(payload.data && payload.data.listen && payload.data.listen.query)) {
-        return;
+        return
       }
-      const { data: { listen: { query: { players: { nodes: playerNodes }}}}} = payload
+      const { data: { listen: { query: { players: { nodes: playerNodes } } } } } = payload
       handlePlayerUpdate(playerNodes)
-    }
-  });
+    },
+  })
 
   const handleLogin = (id) => {
-    getPlayer({ variables: { id } });
-    getRemotePlayers();
-  };
+    getPlayer({ variables: { id } })
+    getRemotePlayers()
+  }
 
-  const handlePlayerUpdate = useCallback((updatedPlayers) => {
-    if (!(state.player && state.player.id)) {
-      return
-    }
-    updatedPlayers.map((updatedPlayer) => {
-      if (updatedPlayer.id !== state.player.id) {
-        dispatch({ type: 'REMOTE_PLAYER_UPDATE_POSITION', payload: updatedPlayer });
+  const handlePlayerUpdate = useCallback(
+    (updatedPlayers) => {
+      if (!(state.player && state.player.id)) {
+        return
       }
-    })
-  }, [dispatch, state.player]);
+      updatedPlayers.map((updatedPlayer) => {
+        if (updatedPlayer.id !== state.player.id) {
+          dispatch({ type: 'REMOTE_PLAYER_UPDATE_POSITION', payload: updatedPlayer })
+        }
+      })
+    },
+    [dispatch, state.player],
+  )
 
   useEffect(() => {
     if (!loadingPlayer && playerData && playerData.player) {
-      dispatch({ type: 'LOGIN', payload: playerData.player });
+      if (!state.player.id) {
+        dispatch({ type: 'LOGIN', payload: playerData.player })
+      }
+      console.log(playerData)
     }
-  }, [loadingPlayer, playerData]);
+  }, [loadingPlayer, playerData])
 
   useEffect(() => {
-
     if (!(state.player && state.player.id)) {
-      return;
+      return
     }
 
     if (!loadingRemotePlayers && remotePlayersData) {
-      dispatch({ type: 'REMOTE_PLAYERS_SET', payload: remotePlayersData.players.nodes.filter(
-        (player) => player.id !== state.player.id
-      )})
+      dispatch({
+        type: 'REMOTE_PLAYERS_SET',
+        payload: remotePlayersData.players.nodes.filter((player) => player.id !== state.player.id),
+      })
     }
-  }, [loadingRemotePlayers, remotePlayersData, state.player]);
+  }, [loadingRemotePlayers, remotePlayersData, state.player])
 
   useEffect(() => {
     const vendorMarkers = state.geofences.filter((geofence) => geofence.type === 'vendor')
@@ -89,7 +106,7 @@ export default function Page(props) {
             id: popupId,
             title: vendor.props.name,
             message: `Press E to interact with ${vendor.props.name}`,
-            vendor
+            vendor,
           },
         })
       }
@@ -99,25 +116,39 @@ export default function Page(props) {
   return (
     <>
       {
-      // state.player.id is truthy when the player is logged in
-      !(state.player && state.player.id) && (
-        <Popup>
-          <Login onLogin={handleLogin} />
-        </Popup>
-      )}
+        // state.player.id is truthy when the player is logged in
+        !(state.player && state.player.id) && (
+          <Popup>
+            <Login onLogin={handleLogin} />
+          </Popup>
+        )
+      }
       {/* Map over the popups in state and render a Popup component for each one */}
       {state.popups.map(({ id, title, message, vendor, interacted }) => (
         <Popup key={id}>
-        <h2>{title}</h2>
-          {interacted && vendor ? 
-          <>
-            <ItemGrid numBoxes={16} items={vendor.markerItems.nodes} />
-          </>
-          :
-          <>
-            <p>{message}</p>
-          </>
-          }
+          <h2>{title}</h2>
+          {/* TODO: Move this to a separate Vendor component along with related gql */}
+          {interacted && vendor ? (
+            <>
+              <ItemGrid
+                numBoxes={16}
+                items={vendor.markerItems.nodes}
+                onItemClick={(item) => {
+                  dispatch({ type: 'UI_POPUP_REMOVE', payload: { id } })
+                  purchaseItem({
+                    variables: {
+                      playerId: parseInt(state.player.id),
+                      itemId: parseInt(item.id),
+                    },
+                  })
+                }}
+              />
+            </>
+          ) : (
+            <>
+              <p>{message}</p>
+            </>
+          )}
         </Popup>
       ))}
     </>
