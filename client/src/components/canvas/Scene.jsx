98c918e1d19ca4056eecbeb8373c87b871721c76
matthaddon <matt@haddon.com.au>
2023-05-21 23:26:04 +1000

    update lock, environment and added effects

--- client/src/components/canvas/Scene.jsx
+++ client/src/components/canvas/Scene.jsx
@@ -1,6 +1,38 @@
+import dynamic from 'next/dynamic'
 import { Canvas } from '@react-three/fiber'
 import { Preload } from '@react-three/drei'
 import KeyboardInput from '../input/KeyboardInput'
+
+const DepthOfField = dynamic(
+  () =>
+    import("@react-three/postprocessing").then((module) => module.DepthOfField),
+  {
+    ssr: false,
+  },
+)
+const Bloom = dynamic(
+  () =>
+    import("@react-three/postprocessing").then((module) => module.Bloom),
+  {
+    ssr: false,
+  },
+)
+const Vignette = dynamic(
+  () =>
+    import("@react-three/postprocessing").then((module) => module.Vignette),
+  {
+    ssr: false,
+  },
+)
+const EffectComposer = dynamic(
+  () =>
+    import("@react-three/postprocessing").then(
+      (module) => module.EffectComposer,
+    ),
+  {
+    ssr: false,
+  },
+)
 
 export default function Scene({ children, ...props }) {
   // Everything defined in here will persist between route changes, only children are swapped
@@ -11,6 +43,12 @@ export default function Scene({ children, ...props }) {
       {children}
       <Preload all />
       <KeyboardInput />
+      <EffectComposer>
+        {/* https://codesandbox.io/embed/r3f-game-i2160  */}
+        <DepthOfField focusDistance={0} focalLength={0.02} bokehScale={2} height={480} />
+        <Bloom luminanceThreshold={0.9} luminanceSmoothing={0.1} height={300} />
+        <Vignette eskil={false} offset={0.1} darkness={1.1} />
+      </EffectComposer>
     </Canvas>
   )
 }
