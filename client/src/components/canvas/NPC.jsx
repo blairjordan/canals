f167841ffdd85e08f369f52b10db9a63b21aab38
Blair Jordan <blair.j.jordan@gmail.com>
2023-08-11 23:11:08 +0800

    feat: add Jimmy npc

--- client/src/components/canvas/NPC.jsx
+++ client/src/components/canvas/NPC.jsx
@@ -2,19 +2,20 @@ import { useGLTF, useAnimations } from '@react-three/drei'
 import { useFrame } from '@react-three/fiber'
 import { useEffect } from 'react'
 
-export function NPC({ position, object, ...props }) {
+export function NPC({ position, rotation, markerKey, ...props }) {
   const { nodes, animations } = useGLTF('/models/characters.glb')
 
   const { ref, mixer } = useAnimations(animations, nodes)
 
   useEffect(() => {
     if (animations.length > 0 && mixer) {
-      const action = mixer.clipAction(animations[animations.length - 1], nodes[object])
+      const animation = animations.find((animation) => animation.name === markerKey)
+      const action = mixer.clipAction(animation, nodes[markerKey])
       action.play()
     }
   }, [animations, mixer])
 
   useFrame((state, delta) => mixer && mixer.update(delta))
 
-  return <primitive object={nodes[object]} position={position} {...props} />
+  return <primitive object={nodes[markerKey]} position={position} rotation={[Math.PI / 2, 0, rotation]} {...props} />
 }
